90119b4a44850070834e79a0f7f9438df46d14cb
Bogdan Gavris <bogdan_gavris@yahoo.com>
2025-12-03 19:33:02 +0200

    Create 01_tables_of_each_schema.sql

---
+++ sql/01_tables_of_each_schema.sql
@@ -0,0 +1,31 @@
+/*Tables by functional area*/
+
+-- Production
+select name as [Production Tables]
+from sys.tables
+where schema_id = schema_id('Production')
+order by name asc
+
+-- Sales
+select name as [Sales Tables]
+from sys.tables
+where schema_id = schema_id('Sales')
+order by name asc
+
+-- HR
+select name as [HR Tables]
+from sys.tables
+where schema_id = schema_id('HumanResources')
+order by name asc
+
+-- Person
+select name as [Person Tables]
+from sys.tables
+where schema_id = schema_id('Person')
+order by name asc
+
+-- Purchasing
+select name as [Purchasing Tables]
+from sys.tables
+where schema_id = schema_id('Purchasing')
+order by name asc
