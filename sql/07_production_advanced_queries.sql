3ee1769bf8d295c010d34e4c5be88f31488c1796
Bogdan Gavris <bogdan_gavris@yahoo.com>
2025-12-13 22:19:05 +0200

    Update 07_production_advanced_queries.sql

--- sql/07_production_advanced_queries.sql
+++ sql/07_production_advanced_queries.sql
@@ -1,14 +1,52 @@
-use AdventureWorks2022
+------------------------------------------------------------------------------------------------------------------------------------------------
 
-/* Product name, category and subcategory  */
-select
-	p.ProductID,
-	p.Name as ProductName,
-	c.Name as Category,
-	sc.Name as Subcategory
-from Production.Product p
-left join Production.ProductSubcategory sc
-	on p.ProductSubcategoryID = sc.ProductSubcategoryID
-left join Production.ProductCategory c
-	on sc.ProductCategoryID = c.ProductCategoryID
--- where c.name is not null
+/* Category-level production metrics query*/
+WITH ProductionCategoryAndSubcategory as
+	(SELECT
+		p.ProductID,
+		p.Name as ProductName,
+		c.Name as Category,
+		sc.Name as Subcategory,
+		StandardCost,
+		ListPrice	
+	FROM Production.Product p
+	left join Production.ProductSubcategory sc				-- Join Product with Category and Subcategory tables
+		ON p.ProductSubcategoryID = sc.ProductSubcategoryID
+	left join Production.ProductCategory c
+		ON sc.ProductCategoryID = c.ProductCategoryID
+),
+CategoryCostAndProfit as
+	(SELECT
+		CASE
+		WHEN Category IS NULL THEN 'Others'				-- Products without assigned category						
+		ELSE Category
+		END AS Category,
+		COUNT(ProductID) as NumberOfProducts,						-- Number of products per category
+		ROUND(AVG(StandardCost),0) AS  AverageStandardCost,			-- Average standard cost per category
+		ROUND(AVG(ListPrice),0) as AverageListPrice,				-- Average list price per category
+		ROUND(SUM(StandardCost),2) AS  TotalCost,
+		ROUND(SUM(ListPrice),2) as TotalPrice,
+		SUM(ListPrice - StandardCost) as TotalProfit,				-- Total profit per category
+		ROUND(SUM(ListPrice - StandardCost) / NULLIF(SUM(ListPrice), 0) * 100, 2) AS ProfitMargin	-- Profit margin (%)
+	FROM ProductionCategoryAndSubcategory
+	GROUP BY Category
+)
+SELECT
+	Category,
+	NumberOfProducts,
+	AverageStandardCost,
+	AverageListPrice,
+	TotalCost,
+	TotalPrice,
+	TotalProfit,
+	ProfitMargin,
+	RANK() OVER (ORDER BY TotalProfit DESC) AS ProfitabilityRank,			-- Ranked the profit using RANK()
+	CASE
+		WHEN TotalProfit > 50000 THEN 'Most Profitable'						-- Profitability flag by profit ranges with CASE
+		WHEN TotalProfit BETWEEN 15000 AND 50000 THEN 'Very Profitable'
+		ELSE 'Profitable'
+		END AS ProfitabilityFlag
+FROM CategoryCostAndProfit
+ORDER BY TotalProfit DESC;
+
+------------------------------------------------------------------------------------------------------------------------------------------------
