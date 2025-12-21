d41c530e91c2db538bc31525277d428782e9071a
Bogdan Gavris <bogdan_gavris@yahoo.com>
2025-12-21 14:43:50 +0200

    Create 08_sales_advanced_queries.sql

---
+++ sql/08_sales_advanced_queries.sql
@@ -0,0 +1,59 @@
+--==============================================================================================================================================
+
+/* Month-over-Month Sales Growth */
+
+WITH YearMonthExtract AS (		-- Extract year-month from order date and keep total sales value
+	SELECT
+		FORMAT(OrderDate, 'yyyy-MM') AS YearMonth,
+		TotalDue
+	FROM Sales.SalesOrderHeader
+),
+MoMSales AS (					-- Aggregate total sales per month
+	SELECT
+		YearMonth,
+		ROUND(SUM(TotalDue), 0) AS TotalSales
+	FROM YearMonthExtract
+	GROUP BY YearMonth
+),
+MoMLag AS (						-- Use LAG() to get previous month's total sales
+	SELECT
+		YearMonth,
+		TotalSales,
+		LAG(TotalSales, 1) OVER (ORDER BY YearMonth) AS TotalSalesLag
+	FROM MoMSales
+)
+SELECT							-- Calculate absolute and percentage Month-over-Month growth
+	YearMonth,
+	TotalSales,
+	TotalSalesLag,
+	TotalSales - TotalSalesLag AS [MoM Sales Difference],
+	ROUND(((TotalSales - TotalSalesLag) / NULLIF(TotalSalesLag, 0)) * 100, 2) AS [MoM Growth (%)]
+FROM MoMLag;
+
+--==============================================================================================================================================
+
+/* YoY Sales Difference */
+
+WITH YoYSales AS (				-- Aggregate total sales per year
+	SELECT
+		YEAR(OrderDate) AS DateYear,
+		ROUND(SUM(TotalDue), 0) AS TotalSales
+	FROM Sales.SalesOrderHeader
+	GROUP BY YEAR(OrderDate)
+),
+YoYLag AS (						-- Use LAG() to compare sales with previous year
+	SELECT
+		DateYear,
+		TotalSales,
+		LAG(TotalSales, 1) OVER (ORDER BY DateYear) AS TotalSalesLag
+	FROM YoYSales
+)
+SELECT							-- Use LAG() to compare sales with previous year
+	DateYear,
+	TotalSales,
+	TotalSalesLag,
+	TotalSales - TotalSalesLag AS [YoY Sales Difference],
+	ROUND(((TotalSales - TotalSalesLag) / NULLIF(TotalSalesLag, 0)) * 100, 2) AS [YoY Growth (%)]
+FROM YoYLag;
+
+--==============================================================================================================================================
